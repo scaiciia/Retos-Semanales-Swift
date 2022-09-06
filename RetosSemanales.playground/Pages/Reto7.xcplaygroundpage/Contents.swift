7a323f976984302713922a7b2278be30658f68e6
Santiago Caiciia Massello <scaiciia@gmail.com>
2022-09-06 00:56:09 -0300

    Reto7 resuelto

---
+++ RetosSemanales.playground/Pages/Reto7.xcplaygroundpage/Contents.swift
@@ -0,0 +1,53 @@
+//: [Previous](@previous)
+
+/*
+ * Reto #7
+ * CONTANDO PALABRAS
+ * Fecha publicación enunciado: 14/02/22
+ * Fecha publicación resolución: 21/02/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Crea un programa que cuente cuantas veces se repite cada palabra y que muestre el recuento final de todas ellas.
+ * - Los signos de puntuación no forman parte de la palabra.
+ * - Una palabra es la misma aunque aparezca en mayúsculas y minúsculas.
+ * - No se pueden utilizar funciones propias del lenguaje que lo resuelvan automáticamente.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */
+
+
+import UIKit
+
+let texto = "Hola, mi nombre es santiago. Mi nombre completo es Santiago Caiciia Massello (pupa)."
+
+
+func contarPalabras(texto: String) {
+    var aux = texto.lowercased()
+    let removeCharacters: Set<Character> = ["(", ")", ",", "."]
+    aux.removeAll(where: { removeCharacters.contains($0) } )
+
+    let arreglo:[Substring] = aux.split(separator: " ")
+    var dic:[String:Int] = [:]
+
+    arreglo.forEach { palabra in
+        let key = String(palabra)
+        if dic[key] != nil {
+            dic[key]! += 1
+        } else {
+            dic[key] = 1
+        }
+    }
+    dic.forEach { (key: String, value: Int) in
+        print(#"La palabra "\#(key)" se repitio \#(value) veces"#)
+    }
+}
+
+contarPalabras(texto: texto)
+
+
+//: [Next](@next)
